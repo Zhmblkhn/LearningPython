0e6446cbe78b8df4edcea073943c755dfdaaae60
Zmblkhn <banichban186@gmail.com>
2026-02-08 23:53:05 +0500

    Some coding without sense

---
+++ fourthWeek/exercise.py
@@ -0,0 +1,28 @@
+# print("1", "2", "3", "4", "5", sep='-', end='!')
+
+# print("Python", "rocks", sep=' ', end='!!!')
+
+# a, b, c, d, e, f = input(), input(), input(), input(), input(), input()
+# print(a, b, c, d, e, f, sep=',', end='!')
+
+# a, b, c = input(), input(), input()
+# print(a, b, c, sep='*', end='.')
+
+# between = input()
+# end = input()
+# a, b, c = input(), input(), input()
+# print(a, b, c, sep=between, end=end)
+
+# num = int(input())
+# print(f'{str(num)} является целым числом')
+
+# a, b = int(input()), int(input())
+# print(a + b)
+# print(a - b)
+# print(a // b)
+# print(a % b)
+
+# a = int(input())
+# print(a % 10)
+# print((a % 100) // 10)  
+# print(a // 100)
